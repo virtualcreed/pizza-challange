88be0c2e02d7278e6c15703e4683c9c8e987a4f4
usama <a2nineu@gmail.com>
2023-10-24 21:19:05 +0500

    order routes changes/order update and button added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,8 @@ class OrdersController < ApplicationController
     def index
         @orders = Order.all
     end
+    def update
+        order = Order.find params[:id]
+        order.update(completed:true)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/orders", to: "orders#index"
+  resources :orders, only:[:index,:update]
 end
