afbb897879a9e1979eb6919f4cfb08d35d9eebd9
usama <a2nineu@gmail.com>
2023-10-24 21:06:22 +0500

    wip order layout/assocation fixes in order

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,9 @@
 module OrdersHelper
+    def pizza_order_ings_formatted(pizza_order_ingredients)
+        result = []
+        pizza_order_ingredients.each do |pizza_order_ingredient|
+            result << pizza_order_ingredient.ingredient.name
+        end
+        result.join(", ")
+    end
 end
